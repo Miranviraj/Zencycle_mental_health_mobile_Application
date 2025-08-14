cc97085e644bb93eb77910ef79feb92edf2d7bc4
Miranviraj <139099074+Miranviraj@users.noreply.github.com>
2025-08-14 09:28:53 +0530

    ok

--- lib/screens/journalscreen.dart
+++ lib/screens/journalscreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import '../services/firestore_service.dart';
 
 class JournalPage extends StatefulWidget {
